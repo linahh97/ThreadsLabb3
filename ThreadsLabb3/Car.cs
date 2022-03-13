42de62527506d9853a0aaa4143e049d55036b2c2
Lina H.H <linah97@hotmail.com>
2022-03-13 22:43:25 +0100

    Add project files.

---
+++ ThreadsLabb3/Car.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ThreadsLabb3
+{
+    public class Car
+    {
+        public string Name { get; set; }
+        public int Speed { get; set; }
+        public int Distance { get; set; }
+    }
+}
